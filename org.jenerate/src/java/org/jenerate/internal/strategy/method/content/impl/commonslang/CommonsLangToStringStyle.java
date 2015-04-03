0ae14629d818405e3e9ba5bae87a85eb5a5c1492
mwensveen <mail@mwensveen.nl>
2015-04-03 21:47:04 +0200

    Add tooltip on ToString dialog Style selection

--- org.jenerate/src/java/org/jenerate/internal/strategy/method/content/impl/commonslang/CommonsLangToStringStyle.java
+++ org.jenerate/src/java/org/jenerate/internal/strategy/method/content/impl/commonslang/CommonsLangToStringStyle.java
@@ -4,38 +4,46 @@ import java.util.EnumSet;
 
 /**
  * Defines the different styles for the commons lang ToStringBuilder
- * 
+ *
  * @author maudrain
  */
 public enum CommonsLangToStringStyle {
 
-    NO_STYLE,
+	NO_STYLE("Person@182f0db[name=John Doe,age=33,smoker=false]"),
 
-    DEFAULT_STYLE,
-    MULTI_LINE_STYLE,
-    NO_FIELD_NAMES_STYLE,
-    SHORT_PREFIX_STYLE,
-    SIMPLE_STYLE;
+	DEFAULT_STYLE(("Person@182f0db[name=John Doe,age=33,smoker=false]")),
+	MULTI_LINE_STYLE("Person@182f0db[\n   name=John Doe\n   age=33\n   smoker=false\n ]"),
+	NO_FIELD_NAMES_STYLE("Person@182f0db[John Doe,33,false]"),
+	SHORT_PREFIX_STYLE("Person[name=John Doe,age=33,smoker=false]"),
+	SIMPLE_STYLE("John Doe,33,false");
 
-    public String getFullStyle() {
-        return CommonsLangMethodContentLibraries.TO_STRING_STYLE + CommonsLangMethodContentLibraries.DOT_STRING
-                + name();
-    }
+	private final String toolTip;
+	private CommonsLangToStringStyle(String toolTip){
+		this.toolTip = toolTip;
 
-    public String getFullLibraryString(boolean useCommonsLang3) {
-        return CommonsLangMethodContentLibraries.COMMONS_LANG_PREFIX
-                + (useCommonsLang3 ? CommonsLangMethodContentLibraries.COMMONS_LANG3_ADDON
-                        : CommonsLangMethodContentLibraries.EMPTY_STRING)
-                + CommonsLangMethodContentLibraries.BUILDER_STRING + getFullStyle();
-    }
+	}
+	public String getFullStyle() {
+		return CommonsLangMethodContentLibraries.TO_STRING_STYLE + CommonsLangMethodContentLibraries.DOT_STRING
+				+ name();
+	}
 
-    public static CommonsLangToStringStyle getToStringStyle(String fullLibraryString) {
-        EnumSet<CommonsLangToStringStyle> enumSet = EnumSet.allOf(CommonsLangToStringStyle.class);
-        for (CommonsLangToStringStyle style : enumSet) {
-            if (fullLibraryString.contains(style.name())) {
-                return style;
-            }
-        }
-        return NO_STYLE;
-    }
+	public String getFullLibraryString(boolean useCommonsLang3) {
+		return CommonsLangMethodContentLibraries.COMMONS_LANG_PREFIX
+				+ (useCommonsLang3 ? CommonsLangMethodContentLibraries.COMMONS_LANG3_ADDON
+						: CommonsLangMethodContentLibraries.EMPTY_STRING)
+						+ CommonsLangMethodContentLibraries.BUILDER_STRING + getFullStyle();
+	}
+
+	public static CommonsLangToStringStyle getToStringStyle(String fullLibraryString) {
+		EnumSet<CommonsLangToStringStyle> enumSet = EnumSet.allOf(CommonsLangToStringStyle.class);
+		for (CommonsLangToStringStyle style : enumSet) {
+			if (fullLibraryString.contains(style.name())) {
+				return style;
+			}
+		}
+		return NO_STYLE;
+	}
+	public String getToolTip() {
+		return toolTip;
+	}
 }
--- org.jenerate/src/java/org/jenerate/internal/ui/dialogs/impl/ToStringDialog.java
+++ org.jenerate/src/java/org/jenerate/internal/ui/dialogs/impl/ToStringDialog.java
@@ -3,7 +3,6 @@ package org.jenerate.internal.ui.dialogs.impl;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
-
 import org.eclipse.jdt.core.IField;
 import org.eclipse.jdt.core.IMethod;
 import org.eclipse.jdt.core.IType;
@@ -11,6 +10,8 @@ import org.eclipse.jdt.core.JavaModelException;
 import org.eclipse.jface.dialogs.Dialog;
 import org.eclipse.jface.dialogs.IDialogSettings;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.ModifyEvent;
+import org.eclipse.swt.events.ModifyListener;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.widgets.Combo;
 import org.eclipse.swt.widgets.Composite;
@@ -26,94 +27,103 @@ import org.jenerate.internal.strategy.method.content.impl.commonslang.CommonsLan
  * @author jiayun
  */
 public class ToStringDialog extends AbstractOrderableFieldDialog<ToStringGenerationData> {
-    
-    public static final String SETTINGS_SECTION = "ToStringDialog";
-    
-    private static final String SETTINGS_STYLE = "CommonsLangToStringStyle";
 
-    private Combo styleCombo;
+	public static final String SETTINGS_SECTION = "ToStringDialog";
 
-    private String toStringStyle;
+	private static final String SETTINGS_STYLE = "CommonsLangToStringStyle";
 
-    private IDialogSettings settings;
+	private Combo styleCombo;
 
-    public ToStringDialog(final Shell parentShell, final String dialogTitle, final IType objectClass,
-            final IField[] fields, final Set<IMethod> excludedMethods, final boolean disableAppendSuper,
-            PreferencesManager preferencesManager, IDialogSettings dialogSettings) throws JavaModelException {
+	private String toStringStyle;
 
-        super(parentShell, dialogTitle, objectClass, fields, excludedMethods, disableAppendSuper, preferencesManager,
-                dialogSettings);
+	private IDialogSettings settings;
 
-        settings = dialogSettings.getSection(SETTINGS_SECTION);
-        if (settings == null) {
-            settings = dialogSettings.addNewSection(SETTINGS_SECTION);
-        }
+	public ToStringDialog(final Shell parentShell, final String dialogTitle, final IType objectClass,
+			final IField[] fields, final Set<IMethod> excludedMethods, final boolean disableAppendSuper,
+			PreferencesManager preferencesManager, IDialogSettings dialogSettings) throws JavaModelException {
 
-        toStringStyle = settings.get(SETTINGS_STYLE);
-        if (toStringStyle == null) {
-            toStringStyle = CommonsLangToStringStyle.NO_STYLE.getFullLibraryString(getPreferencesManager()
-                    .getCurrentPreferenceValue(JeneratePreferences.USE_COMMONS_LANG3).booleanValue());
-        }
-    }
+		super(parentShell, dialogTitle, objectClass, fields, excludedMethods, disableAppendSuper, preferencesManager,
+				dialogSettings);
 
-    @Override
-    public boolean close() {
-        toStringStyle = styleCombo.getText();
-        settings.put(SETTINGS_STYLE, toStringStyle);
-        return super.close();
-    }
+		settings = dialogSettings.getSection(SETTINGS_SECTION);
+		if (settings == null) {
+			settings = dialogSettings.addNewSection(SETTINGS_SECTION);
+		}
 
-    @Override
-    protected Composite createOptionComposite(Composite composite) {
-        Composite optionComposite = super.createOptionComposite(composite);
-        addStyleChoices(optionComposite);
-        return optionComposite;
-    }
+		toStringStyle = settings.get(SETTINGS_STYLE);
+		if (toStringStyle == null) {
+			toStringStyle = CommonsLangToStringStyle.NO_STYLE.getFullLibraryString(getPreferencesManager()
+					.getCurrentPreferenceValue(JeneratePreferences.USE_COMMONS_LANG3).booleanValue());
+		}
+	}
 
-    private Composite addStyleChoices(final Composite composite) {
-        Label label = new Label(composite, SWT.NONE);
-        label.setText("&ToString style:");
+	@Override
+	public boolean close() {
+		toStringStyle = styleCombo.getText();
+		settings.put(SETTINGS_STYLE, toStringStyle);
+		return super.close();
+	}
 
-        GridData data = new GridData(GridData.HORIZONTAL_ALIGN_FILL);
-        label.setLayoutData(data);
+	@Override
+	protected Composite createOptionComposite(Composite composite) {
+		Composite optionComposite = super.createOptionComposite(composite);
+		addStyleChoices(optionComposite);
+		return optionComposite;
+	}
 
-        styleCombo = new Combo(composite, SWT.NONE);
-        boolean useCommonsLang3 = getPreferencesManager().getCurrentPreferenceValue(
-                JeneratePreferences.USE_COMMONS_LANG3).booleanValue();
-        List<String> styles = new ArrayList<String>();
-        for (CommonsLangToStringStyle style : CommonsLangToStringStyle.values()) {
-            if (CommonsLangToStringStyle.NO_STYLE.equals(style)) {
-                styles.add(CommonsLangToStringStyle.NO_STYLE.name());
-            } else {
-                styles.add(style.getFullLibraryString(useCommonsLang3));
-            }
-        }
-        styleCombo.setItems(styles.toArray(new String[styles.size()]));
-        styleCombo.setText(toStringStyle);
+	private Composite addStyleChoices(final Composite composite) {
+		Label label = new Label(composite, SWT.NONE);
+		label.setText("&ToString style:");
 
-        data = new GridData(GridData.FILL_HORIZONTAL);
-        styleCombo.setLayoutData(data);
+		GridData data = new GridData(GridData.HORIZONTAL_ALIGN_FILL);
+		label.setLayoutData(data);
 
-        return composite;
-    }
+		styleCombo = new Combo(composite, SWT.NONE);
+		boolean useCommonsLang3 = getPreferencesManager().getCurrentPreferenceValue(
+				JeneratePreferences.USE_COMMONS_LANG3).booleanValue();
+		List<String> styles = new ArrayList<String>();
+		for (CommonsLangToStringStyle style : CommonsLangToStringStyle.values()) {
+			if (CommonsLangToStringStyle.NO_STYLE.equals(style)) {
+				styles.add(CommonsLangToStringStyle.NO_STYLE.name());
+			} else {
+				styles.add(style.getFullLibraryString(useCommonsLang3));
+			}
+		}
+		styleCombo.setItems(styles.toArray(new String[styles.size()]));
+		styleCombo.setText(toStringStyle);
 
-    @Override
-    public ToStringGenerationData getData() {
-        //@formatter:off
-        return new ToStringGenerationDataImpl.Builder()
-                .withCheckedFields(getCheckedFields())
-                .withElementPosition(getElementPosition())
-                .withAppendSuper(getAppendSuper())
-                .withGenerateComment(getGenerateComment())
-                .withUseBlockInIfStatements(getUseBlockInIfStatements())
-                .withUseGettersInsteadOfFields(getUseGettersInsteadOfFields())
-                .withToStringStyle(CommonsLangToStringStyle.getToStringStyle(toStringStyle))
-                .build();
-        //@formatter:on
-    }
+		data = new GridData(GridData.FILL_HORIZONTAL);
+		styleCombo.setLayoutData(data);
+		styleCombo.setToolTipText(CommonsLangToStringStyle.getToStringStyle(toStringStyle).getToolTip());
 
-    @Override
-    public Dialog getDialog() {
-        return this;
-    }
+		styleCombo.addModifyListener(new ModifyListener() {
+
+			@Override
+			public void modifyText(ModifyEvent event) {
+				Combo combo = (Combo)event.getSource();
+				combo.setToolTipText(CommonsLangToStringStyle.getToStringStyle(combo.getText()).getToolTip());
+			}
+		});
+		return composite;
+	}
+
+	@Override
+	public ToStringGenerationData getData() {
+		//@formatter:off
+		return new ToStringGenerationDataImpl.Builder()
+		.withCheckedFields(getCheckedFields())
+		.withElementPosition(getElementPosition())
+		.withAppendSuper(getAppendSuper())
+		.withGenerateComment(getGenerateComment())
+		.withUseBlockInIfStatements(getUseBlockInIfStatements())
+		.withUseGettersInsteadOfFields(getUseGettersInsteadOfFields())
+		.withToStringStyle(CommonsLangToStringStyle.getToStringStyle(toStringStyle))
+		.build();
+		//@formatter:on
+	}
+
+	@Override
+	public Dialog getDialog() {
+		return this;
+	}
 }
