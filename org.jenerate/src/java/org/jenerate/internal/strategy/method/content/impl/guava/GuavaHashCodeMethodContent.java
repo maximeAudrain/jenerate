faa2f53903338f3fd08b8fa3c723f6c7b52e7a9d
maximeAudrain <maudrain@cern.ch>
2015-04-14 22:30:35 +0200

    Create hashcode method content with guava!

--- org.jenerate/src/java/org/jenerate/internal/strategy/method/content/impl/guava/GuavaHashCodeMethodContent.java
+++ org.jenerate/src/java/org/jenerate/internal/strategy/method/content/impl/guava/GuavaHashCodeMethodContent.java
@@ -4,60 +4,60 @@ import java.util.LinkedHashSet;
 
 import org.eclipse.jdt.core.IField;
 import org.eclipse.jdt.core.IType;
-import org.jenerate.internal.domain.data.ToStringGenerationData;
+import org.jenerate.internal.domain.data.EqualsHashCodeGenerationData;
 import org.jenerate.internal.domain.identifier.StrategyIdentifier;
 import org.jenerate.internal.manage.PreferencesManager;
 import org.jenerate.internal.strategy.method.content.MethodContent;
 import org.jenerate.internal.strategy.method.content.impl.AbstractMethodContent;
 import org.jenerate.internal.strategy.method.content.impl.MethodContentGenerations;
-import org.jenerate.internal.strategy.method.skeleton.impl.ToStringMethodSkeleton;
+import org.jenerate.internal.strategy.method.skeleton.impl.HashCodeMethodSkeleton;
 
 /**
- * Specific implementation of the {@link MethodContent} for {@link ToStringMethodSkeleton} using guava.
+ * Specific implementation of the {@link MethodContent} for {@link HashCodeMethodSkeleton} using guava.
  * 
  * @author maudrain
  */
-public class GuavaToStringMethodContent extends AbstractMethodContent<ToStringMethodSkeleton, ToStringGenerationData> {
+public class GuavaHashCodeMethodContent extends
+        AbstractMethodContent<HashCodeMethodSkeleton, EqualsHashCodeGenerationData> {
 
     /**
      * Public for testing purpose
      */
-    public static final String LIBRARY_TO_IMPORT = "com.google.common.base.MoreObjects";
+    public static final String LIBRARY_TO_IMPORT = "com.google.common.base.Objects";
 
-    public GuavaToStringMethodContent(StrategyIdentifier strategyIdentifier, PreferencesManager preferencesManager) {
+    public GuavaHashCodeMethodContent(StrategyIdentifier strategyIdentifier, PreferencesManager preferencesManager) {
         super(strategyIdentifier, preferencesManager);
     }
 
     @Override
-    public String getMethodContent(IType objectClass, ToStringGenerationData data) throws Exception {
+    public String getMethodContent(IType objectClass, EqualsHashCodeGenerationData data) throws Exception {
         StringBuffer content = new StringBuffer();
-        content.append("return MoreObjects.toStringHelper(this)");
+        content.append("return Objects.hashCode(");
         if (data.getAppendSuper()) {
-            content.append(".add(\"super\", super.toString())");
+            content.append("super.hashCode(), ");
         }
         IField[] checkedFields = data.getCheckedFields();
+        String prefix = "";
         for (IField checkedField : checkedFields) {
-            content.append(".add(\"");
-            content.append(checkedField.getElementName());
-            content.append("\", ");
+            content.append(prefix);
+            prefix = ", ";
             content.append(MethodContentGenerations.getFieldAccessorString(checkedField,
                     data.getUseGettersInsteadOfFields()));
-            content.append(")");
         }
-        content.append(".toString();\n");
+        content.append(");\n");
         return content.toString();
     }
 
     @Override
-    public LinkedHashSet<String> getLibrariesToImport(ToStringGenerationData data) {
+    public LinkedHashSet<String> getLibrariesToImport(EqualsHashCodeGenerationData data) {
         LinkedHashSet<String> toReturn = new LinkedHashSet<String>();
         toReturn.add(LIBRARY_TO_IMPORT);
         return toReturn;
     }
 
     @Override
-    public Class<ToStringMethodSkeleton> getRelatedMethodSkeletonClass() {
-        return ToStringMethodSkeleton.class;
+    public Class<HashCodeMethodSkeleton> getRelatedMethodSkeletonClass() {
+        return HashCodeMethodSkeleton.class;
     }
 
 }
---
+++ org.jenerate/src/test/org/jenerate/internal/strategy/method/content/impl/guava/GuavaHashCodeMethodContentTest.java
@@ -0,0 +1,73 @@
+package org.jenerate.internal.strategy.method.content.impl.guava;
+
+import java.util.Set;
+
+import org.jenerate.internal.domain.data.EqualsHashCodeGenerationData;
+import org.jenerate.internal.domain.identifier.impl.MethodContentStrategyIdentifier;
+import org.jenerate.internal.strategy.method.content.impl.commonslang.AbstractMethodContentTest;
+import org.jenerate.internal.strategy.method.skeleton.impl.HashCodeMethodSkeleton;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+/**
+ * Unit tests for the {@link GuavaHashCodeMethodContent}
+ * 
+ * @author maudrain
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class GuavaHashCodeMethodContentTest extends
+        AbstractMethodContentTest<GuavaHashCodeMethodContent, HashCodeMethodSkeleton, EqualsHashCodeGenerationData> {
+
+    @Override
+    public EqualsHashCodeGenerationData getConcreteData() {
+        return mock(EqualsHashCodeGenerationData.class);
+    }
+
+    @Override
+    public void callbackAfterSetUp() throws Exception {
+        methodContent = new GuavaHashCodeMethodContent(MethodContentStrategyIdentifier.USE_GUAVA, preferencesManager);
+    }
+
+    @Test
+    public void testGetStrategyIdentifier() {
+        assertEquals(MethodContentStrategyIdentifier.USE_GUAVA, methodContent.getStrategyIdentifier());
+    }
+
+    @Test
+    public void testGetRelatedMethodSkeletonClass() {
+        assertEquals(HashCodeMethodSkeleton.class, methodContent.getRelatedMethodSkeletonClass());
+    }
+
+    @Test
+    public void testGetLibrariesToImport() {
+        Set<String> librariesToImport = methodContent.getLibrariesToImport(data);
+        assertEquals(1, librariesToImport.size());
+        assertEquals(GuavaHashCodeMethodContent.LIBRARY_TO_IMPORT, librariesToImport.iterator().next());
+    }
+
+    @Test
+    public void testGetMethodContentDefault() throws Exception {
+        String content = methodContent.getMethodContent(objectClass, data);
+        assertEquals("return Objects.hashCode(field1, field2);\n", content);
+    }
+
+    @Test
+    public void testGetMethodContentWithAppendSuper() throws Exception {
+        when(data.getAppendSuper()).thenReturn(true);
+        String content = methodContent.getMethodContent(objectClass, data);
+        assertEquals("return Objects.hashCode(super.hashCode(), field1, field2);\n", content);
+    }
+
+    @Test
+    public void testGetMethodContentWithUseGettersInsteadOfFields() throws Exception {
+        when(data.getUseGettersInsteadOfFields()).thenReturn(true);
+        String content = methodContent.getMethodContent(objectClass, data);
+        assertEquals("return Objects.hashCode(isField1(), getField2());\n", content);
+    }
+
+}
